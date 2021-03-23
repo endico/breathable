5e2ac5d97bdb51d38743cd00e9ca5966244ab707
Dawn Endico <endico@tafoni.net>
2021-03-23 13:46:08 -0700

    Combine emojis into one sprite sheet

--- code.py
+++ code.py
@@ -1,5 +1,7 @@
 # Based on the RGB Matrix Portal Room CO2 Monitor by Carter Nelson
 # https://learn.adafruit.com/matrix-portal-room-co2-monitor
+# emojis from https://onlineunicodetools.com/convert-emoji-to-image
+# indexed bmp conversion at https://online-converting.com/image/convert2bmp/
 
 import time
 import board
@@ -20,23 +22,29 @@ UPDATE_RATE = 2
 scd30 = adafruit_scd30.SCD30(board.I2C())
 scd30.ambient_pressure = clue.pressure
 
-print("display width", clue.display.width)
-print("display height", clue.display.height)
-print(f'clue.pressure:{clue.pressure}')
-print(f'clue.altitude:{clue.altitude}')
-print(f'clue.sea_level_pressure:{clue.sea_level_pressure}')
-# print(f'dir(clue){dir(clue)}')
-
 text = "        "
 font = bitmap_font.load_font("/SourceSansPro-Black-42.bdf")
 color = 0xFFFFFF
 
 # current condition label
-text_label = label.Label(font, text=text, color=color, x=70, y=120)
-# current CO2 value(good/poor/warn/dang)
-digits_label = label.Label(font, text=text, color=color, x=70, y=160)
+text_label = label.Label(font, text=text, color=color, x=65, y=120)
+# current CO2 value(good/poor/warning/danger)
+digits_label = label.Label(font, text=text, color=color, x=75, y=160)
+# current condition emoji
+bitmap, palette = adafruit_imageload.load("/emojis.bmp")
+emoji = displayio.TileGrid(
+    bitmap,
+    pixel_shader=palette,
+    x=int((clue.display.width / 2) - 37),
+    y=3,
+    width=1,
+    height=1,
+    tile_width=72,
+    tile_height=80,
+)
 
 co2_group = displayio.Group(max_size=4)
+co2_group.append(emoji)
 co2_group.append(text_label)
 co2_group.append(digits_label)
 
@@ -46,40 +54,25 @@ def update_display(value):
 
     if value < CO2_CUTOFFS[0]:
         text_label.text = "GOOD"
-        bitmap, palette = adafruit_imageload.load("/bmps/thumbsup.bmp",
-                                                  bitmap=displayio.Bitmap,
-                                                  palette=displayio.Palette)
+        text_label.x = 65
+        emoji[0] = 0
     elif value < CO2_CUTOFFS[1]:
         text_label.text = "POOR"
-        bitmap, palette = adafruit_imageload.load("/bmps/mask.bmp",
-                                                  bitmap=displayio.Bitmap,
-                                                  palette=displayio.Palette)
+        text_label.x = 65
+        emoji[0] = 1
     elif value < CO2_CUTOFFS[2]:
         text_label.text = "WARNING"
-        bitmap, palette = adafruit_imageload.load("/bmps/scream.bmp",
-                                                  bitmap=displayio.Bitmap,
-                                                  palette=displayio.Palette)
+        text_label.x = 30
+        emoji[0] = 3
     else:
         text_label.text = "DANGER"
-        bitmap, palette = adafruit_imageload.load("/bmps/skull.bmp",
-                                                  bitmap=displayio.Bitmap,
-                                                  palette=displayio.Palette)
-    emoji = displayio.TileGrid(bitmap, pixel_shader=palette)
-    emoji.x = int( (clue.display.width / 2) - 37 )
-    emoji.y = 3
-    co2_group.append(emoji)
-clue.display.show(co2_group)
+        text_label.x = 40
+        emoji[0] = 4
 
-print("scd30.temperature:", scd30.temperature, "degrees C")
-print("scd30.relative_humidity:", scd30.relative_humidity, "%rH")
-print("scd30.altitude:", scd30.altitude, "")
-print("scd30.ambient_pressure:", scd30.ambient_pressure)
+update_display(scd30.CO2)
+clue.display.show(co2_group)
 
 while True:
     if scd30.data_available:
-        # protect against NaNs and Nones
-        try:
-            update_display(scd30.CO2)
-        except:
-            pass
-        time.sleep(UPDATE_RATE)
+        update_display(scd30.CO2)
+    time.sleep(UPDATE_RATE)
